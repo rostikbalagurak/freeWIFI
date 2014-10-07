712f837ef00b79295a4407e639ff054b378d3cdc
rba <rba@interlogic.com.ua>
2014-10-07 19:56:47 +0300

    change sql structure, fix WifiSpot model, fix one-to-many relationships, implement few wificontroller methods

--- app/controllers/WifiController.php
+++ app/controllers/WifiController.php
@@ -9,31 +9,66 @@ class WifiController extends BaseController {
 	}
 
 
-	public function createAction($data){
-		$result = ResponseMessage::OK;
-		try{
-			$wifiSpot = new WifiSpot();
-			$wifiSpot->fill($data);
-			if($wifiSpot->save()){
+	public function createAction(){
+		$responseData = array();
+		if(!$this->request->isPost()){
+			$this->setBadRequestStatus();
+		} else {
+			$this->setOkStatus();
+			try{
+				$wifiSpot = new WifiSpot();
+				$wifiSpot->setOwnerId($this->request->getPost(WifiSpot::COL_OWNER_ID));
+				$wifiSpot->setPassword($this->request->getPost(WifiSpot::COL_PASSWORD), null, '');
+				$wifiSpot->setName($this->request->getPost(WifiSpot::COL_NAME));
+				$wifiSpot->setAddress($this->request->getPost(WifiSpot::COL_ADDRESS));
+				$wifiSpot->setPopularity($this->request->getPost(WifiSpot::COL_POPULARITY));
+				$wifiSpot->setLat($this->request->getPost(WifiSpot::COL_LAT));
+				$wifiSpot->setLng($this->request->getPost(WifiSpot::COL_LNG));
+				
+				$wifiSpot->save();
 				$responseData = array(
-					'result' => $result,
+					'result' => ResponseMessage::OK,
 					'spot_id' => $wifiSpot->getSpotId(),
 				);
-			} else {
-				$responseData = array(
-					'result' => ResponseMessage::INTERNAL_ERROR
-				);
+			} catch(Exception $e){
+				$this->handleError($e->getMessage());
+				return;
 			}
-			
-			$this->setOkStatus();
-		} catch (Exception $e){
-			$this->setInternalErrorStatus();
-			
-			$result = ResponseMessage::INTERNAL_ERROR;
-			$responseData = array(
-				'result' => $result,
-			);
 		}
+
 		$this->sendResponse($responseData);
 	}
+	
+	public function getSingleAction($spot_id){
+		$wifiSpot = WifiSpot::findFirst($spot_id);
+		if(!empty($wifiSpot)){
+			$wifiSpot = $wifiSpot->asArray();
+		}
+		
+		$responseData = array(
+			'wifi_spot' => $wifiSpot
+		);
+		$this->setOkStatus();
+		$this->sendResponse($responseData);
+	}
+
+	/**
+	 * @param $filters
+	 * Param should assoc array in json format
+	 */
+	public function getAction($filters){
+		$filters = json_decode($filters, true);
+		$owner_id = isset($filters[WifiSpot::COL_OWNER_ID]) ? $filters[WifiSpot::COL_OWNER_ID] : null;
+		
+		$owner = User::findFirst($owner_id);
+		$res = $owner->WifiSpot->toArray();
+
+		$responseData = array(
+			'wifi_spots' => $res
+		);
+		$this->setOkStatus();
+		$this->sendResponse($responseData);
+	}
+	
+	
 }
