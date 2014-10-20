c9181b8d3f662eafe28f8d1ad9498c3bc5ee3455
rba <rba@interlogic.com.ua>
2014-10-20 19:44:18 +0300

    test change 2

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -10,27 +10,29 @@ class UserController extends BaseController
 	}
 
 	public function getAction($email, $password){
-		$app = $this->getDI()->get('app');
-		$salt = $app->auth->salt;
-		
-		$user = User::get($email, md5($password . $salt));
-		if($user){
-			$responseData = array(
-				'result' => ResponseMessage::OK,
-				'user_id' => $user->getUserId(),
-				'firstname' => $user->getFirstname(),
-				'lastname' => $user->getLastname(),
-				'email' => $user->getEmail(),
-			);
-
-			$this->setOkStatus();
-		} else {
-			$this->setNotFoundStatus();
-			$responseData = array(
-				'result' => ResponseMessage::USER_NOT_FOUND,
-			);
-		}
-		$this->sendResponse($responseData);
+		$user = new User();
+		print_r($user);
+//		$app = $this->getDI()->get('app');
+//		$salt = $app->auth->salt;
+//
+//		$user = User::get($email, md5($password . $salt));
+//		if($user){
+//			$responseData = array(
+//				'result' => ResponseMessage::OK,
+//				'user_id' => $user->getUserId(),
+//				'firstname' => $user->getFirstname(),
+//				'lastname' => $user->getLastname(),
+//				'email' => $user->getEmail(),
+//			);
+//
+//			$this->setOkStatus();
+//		} else {
+//			$this->setNotFoundStatus();
+//			$responseData = array(
+//				'result' => ResponseMessage::USER_NOT_FOUND,
+//			);
+//		}
+//		$this->sendResponse($responseData);
 	}
 	
 	public function createAction(){
